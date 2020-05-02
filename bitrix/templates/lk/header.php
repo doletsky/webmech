74366f33741cd77700fa0e973ec043ea467452ae
doletsky <doletsky@bk.ru>
2020-05-02 18:45:52 +0300

    edit site name

--- bitrix/templates/lk/header.php
+++ bitrix/templates/lk/header.php
@@ -26,9 +26,9 @@ if ($USER->IsAdmin() && $_GET["panel"]==1)$APPLICATION->ShowPanel();
     <div class="container lk-students">
         <div class="row">
             <div class="col-md-2">
-                <a class="logo-header">
-                    <span class="pull-right">ONLINE</span>
-                    <h4><span class="colored">PRACTICUM</span></h4>
+                <a class="logo-header" style="position: relative;width: 270px;height: 70px;">
+                    <span class="pull-right">АНТИШКОЛА</span>
+                    <h3><span class="colored" style="position: absolute;line-height: 22px;letter-spacing: 7px;font-size: 90%;top: 10px;">САМ&nbspСЕБЕ<div style="letter-spacing: normal">УНИВЕРСИТЕТ</div></span></h3>
                     <div class="clearfix"></div>
                     <hr>
                 </a>
