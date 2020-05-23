78b847182bc11b5fa8bf94565859cb5f6cfd4894
doletsky <doletsky@bk.ru>
2020-05-23 11:23:36 +0300

    адменика - подсказака иконки в ИБ Выгоды

--- bitrix/php_interface/admin_header.php
+++ bitrix/php_interface/admin_header.php
@@ -1,13 +1,11 @@
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="../templates/forque/js/jquery.min.js"></script>
+
 <script>
     $(document).ready(function(){
-        if($('#edit1 .adm-input-file-exist-cont').length>0){
-            var cnt=1;
-            $('#edit1 .adm-input-file-exist-cont').each(function(){
-                $(this).children(':first').before('<div style="float:left;font-size: 200%;">'+cnt+'.</div>');
-                cnt++;
-            });
-        }
+        if($('#tr_PROPERTY_771 input').val().length>0){
+            $('#tr_PROPERTY_771 input').before('<div class="box-icon icon x2 colorful-icon mr-20"><i class="'+$('#tr_PROPERTY_771 input').val()+'"></i></div>');
+//            $('<link href="../templates/forque/css/admin_header_add.css" rel="stylesheet" type="text/css">').appendTo('head');
+            $('<style>.icon-hotairballoon:before {content: "\\e044";}</style>').appendTo('head');
+            };
     });
 </script>
-//adm-detail-content-table edit-table
