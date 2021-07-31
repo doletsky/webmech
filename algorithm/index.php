106253fcbda7ec1a500081f66e6c07e2cd6a5282
Александр <doletsky@bk.ru>
2021-07-31 17:24:38 +0400

    add Algorithm page & jordan.css

---
+++ algorithm/index.php
@@ -0,0 +1,67 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Алгоритм"); ?>
+<?$APPLICATION->AddHeadString('<link href="'.$APPLICATION->GetCurDir().'"jordan.css";  type="text/css" rel="stylesheet" />',true)?>
+    <section id="experience-education" class="section-experience-education bgPrimaryColor">
+        <svg class="holes" viewBox="0 0 1189 22" preserveAspectRatio="xMinYMin meet"><circle class="cls-1" cx="37.15" cy="11" r="11"></circle><circle class="cls-1" cx="74.31" cy="11" r="11"></circle><circle class="cls-1" cx="111.46" cy="11" r="11"></circle><circle class="cls-1" cx="148.61" cy="11" r="11"></circle><circle class="cls-1" cx="185.77" cy="11" r="11"></circle><circle class="cls-1" cx="222.92" cy="11" r="11"></circle><circle class="cls-1" cx="260.07" cy="11" r="11"></circle><circle class="cls-1" cx="297.23" cy="11" r="11"></circle><circle class="cls-1" cx="334.38" cy="11" r="11"></circle><circle class="cls-1" cx="371.54" cy="11" r="11"></circle><circle class="cls-1" cx="408.69" cy="11" r="11"></circle><circle class="cls-1" cx="445.84" cy="11" r="11"></circle><circle class="cls-1" cx="483" cy="11" r="11"></circle><circle class="cls-1" cx="520.15" cy="11" r="11"></circle><circle class="cls-1" cx="557.3" cy="11" r="11"></circle><circle class="cls-1" cx="594.46" cy="11" r="11"></circle><circle class="cls-1" cx="631.61" cy="11" r="11"></circle><circle class="cls-1" cx="668.76" cy="11" r="11"></circle><circle class="cls-1" cx="705.92" cy="11" r="11"></circle><circle class="cls-1" cx="743.07" cy="11" r="11"></circle><circle class="cls-1" cx="780.22" cy="11" r="11"></circle><circle class="cls-1" cx="817.38" cy="11" r="11"></circle><circle class="cls-1" cx="854.53" cy="11" r="11"></circle><circle class="cls-1" cx="891.69" cy="11" r="11"></circle><circle class="cls-1" cx="928.84" cy="11" r="11"></circle><circle class="cls-1" cx="965.99" cy="11" r="11"></circle><circle class="cls-1" cx="1003.15" cy="11" r="11"></circle><circle class="cls-1" cx="1040.3" cy="11" r="11"></circle><circle class="cls-1" cx="1077.45" cy="11" r="11"></circle><circle class="cls-1" cx="1114.61" cy="11" r="11"></circle><circle class="cls-1" cx="1151.76" cy="11" r="11"></circle><path class="cls-1" d="M1253,389a11,11,0,0,0,0,22V389Z" transform="translate(-64 -389)"></path><path class="cls-1" d="M64,389v22A11,11,0,0,0,64,389Z" transform="translate(-64 -389)"></path></svg>
+        <h2>Experience &amp; Education</h2>
+        <ul class="timeline">
+            <li>
+                <div class="icon"><img src="img/suitcase.svg" alt=""></div>
+                <div class="animation-chain overflow-hidden" data-animation="fadeInLeft">
+                    <h3 class="animated" style="animation-delay: 0s;">UI / UX Designer</h3>
+                    <h4 class="animated" style="animation-delay: 0.1s;">Amazing Design</h4>
+                    <div class="date animated" style="animation-delay: 0.2s;">Aug 2007 - Now</div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta exercitationem nam perferendis tempora eius repellat expedita numquam, beatae eaque nihil? Aliquam architecto voluptatum provident animi, excepturi fugit ut! Impedit, eum!</p>
+            </li>
+            <li>
+                <div class="icon"><img src="img/suitcase.svg" alt=""></div>
+                <div class="animation-chain overflow-hidden" data-animation="fadeInRight">
+                    <h3 class="animated" style="animation-delay: 0s;">Front-End Web Developer</h3>
+                    <h4 class="animated" style="animation-delay: 0.1s;">AudioJungle</h4>
+                    <div class="date animated" style="animation-delay: 0.2s;">Aug 2007 - Jan 2010</div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id error atque debitis tempora soluta, modi voluptatum sunt optio, ut quidem quam dolore. Unde minus quasi consequuntur consequatur, tempore repudiandae aperiam?</p>
+            </li>
+            <li>
+                <div class="icon"><img src="img/cap.svg" alt=""></div>
+                <div class="animation-chain overflow-hidden" data-animation="fadeInLeft">
+                    <h3 class="animated" style="animation-delay: 0s;">Professional Diploma in Graphic Design </h3>
+                    <h4 class="animated" style="animation-delay: 0.1s;">Beautiful College</h4>
+                    <div class="date animated" style="animation-delay: 0.2s;">Sep 2012 - Aug 2013</div>
+                </div>
+            </li>
+            <li>
+                <div class="icon"><img src="img/cap.svg" alt=""></div>
+                <div class="animation-chain overflow-hidden" data-animation="fadeInRight">
+                    <h3 class="animated" style="animation-delay: 0s;">Master of Engineering in Computer Engineering</h3>
+                    <h4 class="animated" style="animation-delay: 0.1s;">Fantastic University</h4>
+                    <div class="date animated" style="animation-delay: 0.2s;">Sep 2010 - Aug 2012</div>
+                </div>
+                <p>Thesis: Nulla, Omnis Vitae Illum Molestiae Rem<br>GPA: 4.0</p>
+            </li>
+            <li>
+                <div class="icon"><img src="img/suitcase.svg" alt=""></div>
+                <div class="animation-chain overflow-hidden" data-animation="fadeInLeft">
+                    <h3 class="animated" style="animation-delay: 0s;">Back-End Web Developer</h3>
+                    <h4 class="animated" style="animation-delay: 0.1s;">PhotoDune</h4>
+                    <div class="date animated" style="animation-delay: 0.2s;">Aug 2007 - Jan 2010</div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id error atque debitis tempora soluta, modi voluptatum sunt optio, ut quidem quam dolore. Unde minus quasi consequuntur consequatur, tempore repudiandae aperiam?</p>
+            </li>
+            <li>
+                <div class="icon"><img src="img/cap.svg" alt=""></div>
+                <div class="animation-chain overflow-hidden" data-animation="fadeInRight">
+                    <h3 class="animated" style="animation-delay: 0s;">Bachelor of Engineering in Computer Science</h3>
+                    <h4 class="animated" style="animation-delay: 0.1s;">Incredible University</h4>
+                    <div class="date animated" style="animation-delay: 0.2s;">Aug 2005 - Aug 2009</div>
+                </div>
+                <p>First-class honors<br>GPA: 4.33</p>
+            </li>
+        </ul>
+        <svg class="waves" viewBox="0 0 1149 39.29" preserveaspectration="none"><path d="M66,378.29v9a49.32,49.32,0,0,1,35.26,14.58l1.05,1.1A50.12,50.12,0,0,0,173,403l1.09-1.1a50.14,50.14,0,0,1,70.7,0l1.1,1.1a50.15,50.15,0,0,0,70.71,0l1.1-1.1a50.15,50.15,0,0,1,70.71,0l1.1,1.1a50.15,50.15,0,0,0,70.71,0l1.1-1.1a50.15,50.15,0,0,1,70.71,0l1.1,1.1a50.15,50.15,0,0,0,70.71,0l1.1-1.1a50.15,50.15,0,0,1,70.71,0l1.1,1.1a50.15,50.15,0,0,0,70.71,0l1.1-1.1a50.15,50.15,0,0,1,70.71,0l1.1,1.1a50.15,50.15,0,0,0,70.71,0l1.1-1.1a50.15,50.15,0,0,1,70.71,0L964,403a50.15,50.15,0,0,0,70.71,0l1.1-1.1a50.15,50.15,0,0,1,70.71,0l1.1,1.1a50.15,50.15,0,0,0,70.71,0l1.2-1.1A49.75,49.75,0,0,1,1215,387.33v-9H66Z" transform="translate(-66 -378.29)"></path></svg>
+    </section>
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+?>
