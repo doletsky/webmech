1d67d8c663535c02ac94b9270c6a67badda51cd4
Александр <doletsky@bk.ru>
2021-09-03 20:31:15 +0400

    Algorithm page steps & title

--- algorithm/index.php
+++ algorithm/index.php
@@ -1,15 +1,15 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Алгоритм"); ?>
+$APPLICATION->SetTitle("Алгоритм самообучения"); ?>
 <?$APPLICATION->AddHeadString('<link href="'.SITE_TEMPLATE_PATH.'/css/jordan.css";  type="text/css" rel="stylesheet" />',true)?>
     <section id="experience-education" class="section-experience-education bgPrimaryColor">
-        <h2>Experience &amp; Education</h2>
+        <h2>Самообучение: пошаговый план</h2>
         <ul class="timeline">
             <li>
                 <div class="icon"><img src="img/suitcase.svg" alt=""></div>
                 <div class="animation-chain overflow-hidden" data-animation="fadeInLeft">
                     <div class="date animated" style="animation-delay: 0.2s;">Шаг первый</div>
-                    <h3 class="animated" style="animation-delay: 0s;">Определение цели.</h3>
+                    <h3 class="animated" style="animation-delay: 0s;">Цель</h3>
                     <h4 class="animated" style="animation-delay: 0.1s;">Формулирование желаемого результата</h4>
 
                 </div>
@@ -18,46 +18,48 @@ $APPLICATION->SetTitle("Алгоритм"); ?>
             <li>
                 <div class="icon"><img src="img/suitcase.svg" alt=""></div>
                 <div class="animation-chain overflow-hidden" data-animation="fadeInRight">
-                    <h3 class="animated" style="animation-delay: 0s;">Front-End Web Developer</h3>
-                    <h4 class="animated" style="animation-delay: 0.1s;">AudioJungle</h4>
-                    <div class="date animated" style="animation-delay: 0.2s;">Aug 2007 - Jan 2010</div>
+                    <div class="date animated" style="animation-delay: 0.2s;">Шаг второй</div>
+                    <h3 class="animated" style="animation-delay: 0s;">Источники</h3>
+                    <h4 class="animated" style="animation-delay: 0.1s;">Определение и анализ источников.</h4>
                 </div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id error atque debitis tempora soluta, modi voluptatum sunt optio, ut quidem quam dolore. Unde minus quasi consequuntur consequatur, tempore repudiandae aperiam?</p>
             </li>
             <li>
                 <div class="icon"><img src="img/cap.svg" alt=""></div>
                 <div class="animation-chain overflow-hidden" data-animation="fadeInLeft">
-                    <h3 class="animated" style="animation-delay: 0s;">Professional Diploma in Graphic Design </h3>
-                    <h4 class="animated" style="animation-delay: 0.1s;">Beautiful College</h4>
-                    <div class="date animated" style="animation-delay: 0.2s;">Sep 2012 - Aug 2013</div>
+                    <div class="date animated" style="animation-delay: 0.2s;">Шаг третий</div>
+                    <h3 class="animated" style="animation-delay: 0s;">Контроль</h3>
+                    <h4 class="animated" style="animation-delay: 0.1s;">Определение способов контроля.</h4>
                 </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id error atque debitis tempora soluta, modi voluptatum sunt optio, ut quidem quam dolore. Unde minus quasi consequuntur consequatur, tempore repudiandae aperiam?</p>
             </li>
             <li>
                 <div class="icon"><img src="img/cap.svg" alt=""></div>
                 <div class="animation-chain overflow-hidden" data-animation="fadeInRight">
-                    <h3 class="animated" style="animation-delay: 0s;">Master of Engineering in Computer Engineering</h3>
-                    <h4 class="animated" style="animation-delay: 0.1s;">Fantastic University</h4>
-                    <div class="date animated" style="animation-delay: 0.2s;">Sep 2010 - Aug 2012</div>
+                    <div class="date animated" style="animation-delay: 0.2s;">Шаг четвертый</div>
+                    <h3 class="animated" style="animation-delay: 0s;">План</h3>
+                    <h4 class="animated" style="animation-delay: 0.1s;">Составление этапов обучения.</h4>
                 </div>
                 <p>Thesis: Nulla, Omnis Vitae Illum Molestiae Rem<br>GPA: 4.0</p>
             </li>
             <li>
                 <div class="icon"><img src="img/suitcase.svg" alt=""></div>
                 <div class="animation-chain overflow-hidden" data-animation="fadeInLeft">
-                    <h3 class="animated" style="animation-delay: 0s;">Back-End Web Developer</h3>
-                    <h4 class="animated" style="animation-delay: 0.1s;">PhotoDune</h4>
-                    <div class="date animated" style="animation-delay: 0.2s;">Aug 2007 - Jan 2010</div>
+                    <div class="date animated" style="animation-delay: 0.2s;">Шаг пятый</div>
+                    <h3 class="animated" style="animation-delay: 0s;">Среда</h3>
+                    <h4 class="animated" style="animation-delay: 0.1s;">Создание обстоятельств, обеспечивающих выполнение плана</h4>
                 </div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id error atque debitis tempora soluta, modi voluptatum sunt optio, ut quidem quam dolore. Unde minus quasi consequuntur consequatur, tempore repudiandae aperiam?</p>
             </li>
             <li>
                 <div class="icon"><img src="img/cap.svg" alt=""></div>
                 <div class="animation-chain overflow-hidden" data-animation="fadeInRight">
-                    <h3 class="animated" style="animation-delay: 0s;">Bachelor of Engineering in Computer Science</h3>
-                    <h4 class="animated" style="animation-delay: 0.1s;">Incredible University</h4>
-                    <div class="date animated" style="animation-delay: 0.2s;">Aug 2005 - Aug 2009</div>
+                    <div class="date animated" style="animation-delay: 0.2s;">Последний шаг</div>
+                    <h3 class="animated" style="animation-delay: 0s;">Старт</h3>
+                    <h4 class="animated" style="animation-delay: 0.1s;">Запуск процесса обучения</h4>
+
                 </div>
-                <p>First-class honors<br>GPA: 4.33</p>
+<!--                <p>First-class honors<br>GPA: 4.33</p>-->
             </li>
         </ul>
         <svg class="waves" viewBox="0 0 1149 39.29" preserveaspectration="none"><path d="M66,378.29v9a49.32,49.32,0,0,1,35.26,14.58l1.05,1.1A50.12,50.12,0,0,0,173,403l1.09-1.1a50.14,50.14,0,0,1,70.7,0l1.1,1.1a50.15,50.15,0,0,0,70.71,0l1.1-1.1a50.15,50.15,0,0,1,70.71,0l1.1,1.1a50.15,50.15,0,0,0,70.71,0l1.1-1.1a50.15,50.15,0,0,1,70.71,0l1.1,1.1a50.15,50.15,0,0,0,70.71,0l1.1-1.1a50.15,50.15,0,0,1,70.71,0l1.1,1.1a50.15,50.15,0,0,0,70.71,0l1.1-1.1a50.15,50.15,0,0,1,70.71,0l1.1,1.1a50.15,50.15,0,0,0,70.71,0l1.1-1.1a50.15,50.15,0,0,1,70.71,0L964,403a50.15,50.15,0,0,0,70.71,0l1.1-1.1a50.15,50.15,0,0,1,70.71,0l1.1,1.1a50.15,50.15,0,0,0,70.71,0l1.2-1.1A49.75,49.75,0,0,1,1215,387.33v-9H66Z" transform="translate(-66 -378.29)"></path></svg>
