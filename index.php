71e69ffed445467fde8a29c7c1fa21fa8ef750b4
Александр <doletsky@bk.ru>
2021-09-03 14:53:56 +0400

    tune Algorithm page & jordan.css

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-//$APPLICATION->SetTitle("Индигос.ru — магазин образовательного контента");
+$APPLICATION->SetTitle("Сам себе университет");
 ?>
 
 
