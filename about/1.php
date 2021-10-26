a2b7b35d108e1b648f333e34e6184d62667b1a67
User <doletsky@bk.ru>
2021-10-27 01:49:08 +0300

    test vk stream

--- about/1.php
+++ about/1.php
@@ -1,28 +1,21 @@
 <?
-$BXID='8b6f5cf6639ac5557a88e0f15a0807dc0';
-$LCHAT_HASH='7c1f1cf51100c68da64223a4b06c5b65';
-setcookie ("BX_USER_ID", $BXID, time()+3600,"/", "practicumonline.bitrix24.ru");
-setcookie ("LIVECHAT_HASH", $LCHAT_HASH, time()+3600, "/", "practicumonline.bitrix24.ru");
-header("Location: https://practicumonline.bitrix24.ru/online/kzs");exit();
-//@error_reporting(E_ALL);
-//@ini_set('error_reporting', E_ALL);
-//
-//@header("Content-type: text/html; charset=UTF-8");
+define('ACCESS_TOKEN', '0c70c20d668015042f6b01bc5e7ccb513065c0bb49e4f763fcad7fc204e97e842df0842206a6186259293');
 
+function api($method, $params = array())
+{
+    $params['access_token'] = ACCESS_TOKEN;
 
+    $url = 'https://api.vk.com/method/' . $method . '?' . http_build_query($params);
+    $response = file_get_contents($url);
+    return json_decode($response, true);
+}
 
-//$ch = curl_init('https://practicumonline.bitrix24.ru/online/kzs');
-//curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8 ( .NET CLR 3.5.30729)');
-//curl_setopt($ch, CURLOPT_HEADER, 0);
-//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//curl_setopt($ch, CURLOPT_TIMEOUT, 120);
-//curl_setopt($ch, CURLOPT_REFERER, $refer);
-//$page = curl_exec($ch);
-//curl_close($ch);
-//$rPage=str_replace('href="/bitrix/','href="https://practicumonline.bitrix24.ru/bitrix/', $page);
-//file_put_contents(getcwd() . '/hook.html', $rPage);
-?>
+$api = api('video.get', array(
+    'videos' => '683028574_456239018', 'v'=>'5.131'
+));
+?><pre><?
+print_r($api);
+?></pre>
 <!---->
 <!--</body>-->
 <!--</html>-->
